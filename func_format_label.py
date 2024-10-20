cbb626743ca5776da48540f4bd3e3245b41a023a
MD-Shahriar-Mahmud <43397680+MD-Shahriar-Mahmud@users.noreply.github.com>
2024-10-20 01:44:16 -0500

    Delete func_format_label.py

--- func_format_label.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun  6 19:54:03 2024
-
-@author: shahriar
-"""
-
-def format_label(param_name):
-    return r'${}$'.format(param_name) if param_name in {'c', 'k','R_0'} else r'$\{}$'.format(param_name)
